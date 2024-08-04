d36dfcf460ece601bcde4d6e0ce3f08533f9401d
AbubakirMahkamov-prog <mahkamovabubakir65119@gmail.com>
2024-08-04 09:37:41 +0500

    dsf

--- src/components/my-components/TaskForm.tsx
+++ src/components/my-components/TaskForm.tsx
@@ -64,7 +64,7 @@ export default function ProfileForm({ onSuccess, id }: ProfileFormProps) {
     } else {
       taskService.updateItem(id, data).then(() => {
         onSuccess()
-      })
+      });
     }
   };
 
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -10,86 +10,47 @@ import {
   } from "@/components/ui/table"
   import Layout from "./Layout"
   import * as taskService from '../services/taskService'
-import { useEffect } from "react"
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
-  
+import { useEffect, useState } from "react"
+interface IResult {
+    _id: string;
+    fullname: string;
+    newTasksCount: number;
+    completedTasksCount: number;
+    totalTasksCount: number;
+    completionPercentage: number;
+} 
+
   export default function TableDemo() {
+    const [results, setResults] = useState<IResult[]>([]);
     useEffect(() => {
-        taskService.getStatistics().then((res) => {
-            console.log(res)
+        taskService.getStatistics().then((res: IResult[]) => {
+            setResults(res)
         })
     }, [])
     return (
     <Layout>
-        
-        <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
+        <p className="py-4 border-b-2">This table contains statistics of users</p>
+        <Table className="mt-2">
+            <TableCaption>Statistics</TableCaption>
             <TableHeader>
             <TableRow>
-                <TableHead className="w-[100px]">Invoice</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+                <TableHead>Fullname</TableHead>
+                <TableHead className="w-[200px] text-right">Completed task count</TableHead>
+                <TableHead className="w-[200px] text-right">New task count</TableHead>
+                <TableHead className="w-[300px] text-right">Percentage of completed task</TableHead>
             </TableRow>
             </TableHeader>
             <TableBody>
-            {invoices.map((invoice) => (
-                <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+            {results.map((result) => (
+                <TableRow key={result._id}>
+                <TableCell className="font-medium">{result.fullname}</TableCell>
+                <TableCell className="text-right">{result.completedTasksCount}</TableCell>
+                <TableCell className="text-right">{result.newTasksCount}</TableCell>
+                <TableCell className="text-right">{result.completionPercentage}</TableCell>
                 </TableRow>
             ))}
             </TableBody>
             <TableFooter>
-            <TableRow>
-                <TableCell colSpan={3}>Total</TableCell>
-                <TableCell className="text-right">$2,500.00</TableCell>
-            </TableRow>
             </TableFooter>
         </Table>
     </Layout>
